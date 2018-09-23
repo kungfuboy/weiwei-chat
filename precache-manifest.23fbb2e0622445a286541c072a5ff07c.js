e4d1ab82b40b98a64c993848df8bb992ba133d9d
songjunjie <songjunjie@advanpro.hk>
2018-09-23 13:59:57 +0800

    deploy

---
+++ precache-manifest.23fbb2e0622445a286541c072a5ff07c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weiwei-chat/robots.txt"
+  },
+  {
+    "revision": "7253e3206b7e993947ba",
+    "url": "/weiwei-chat/js/chunk-vendors.55fcb56e.js"
+  },
+  {
+    "revision": "950756427d0c441771e6",
+    "url": "/weiwei-chat/js/app.3f2a056a.js"
+  },
+  {
+    "revision": "515e9bd203f270131413949ade6956bb",
+    "url": "/weiwei-chat/index.html"
+  },
+  {
+    "revision": "49767ac507f1808d28b1122464ebb4e1",
+    "url": "/weiwei-chat/img/texture.49767ac5.png"
+  },
+  {
+    "revision": "950756427d0c441771e6",
+    "url": "/weiwei-chat/css/app.38870e72.css"
+  }
+];
